57bf84f9c098eca7a78ba3cb30308df2906557ba
MohammedL-dev <mohammedlamharzi@gmail.com>
2025-06-04 21:44:19 +0200

    project

---
+++ 50_project/form-wave/script.js
@@ -0,0 +1,11 @@
+const user = document.querySelectorAll('.user > span');
+const password = document.querySelectorAll('.password > span');
+const in_user = document.getElementById('user');
+const in_password = document.getElementById('password');
+
+
+
+// dont need to
+
+
+
